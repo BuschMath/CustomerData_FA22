157713ed9ec996bf61e1abac9ac5df282338e5ee
BuschMath <busch@iowacentral.edu>
2022-09-02 10:46:41 -0500

    Age calculation

--- CustomerData_FA22/main.cpp
+++ CustomerData_FA22/main.cpp
@@ -1,20 +1,56 @@
+/*
+	Created by David Busch
+	Sept. 2nd, 2022
+	Customer Data Program Demo
+*/
+
 #include <iostream>
 #include <string>
 
 using namespace std;
 
+// Customer names
+string fName;
+string lName;
+
+string birthMonth;
+string birthDay;
+string birthYear;
+
+// Precondition: Include iostream library
+// Postcondition: Collected customer name data
+void InputName();
+
+// Precondition: Include iostream library
+// Postcondition: Collected customer birthday data
+void InputBirthDay();
+
 int main()
 {
-	string fName;
-	string lName;
+	InputName();
+	InputBirthDay();
 
-	cout << "Please enter your first name: ";
-	cin >> fName;
+	int age = 2022 - stoi(birthYear);
 
-	cout << "\nPlease enter your last name: ";
-	cin >> lName;
-
-	cout << "\nSo your name is " << fName << " " << lName;
+	cout << "\nYour age is " << age;
 
 	return 0;
+}
+
+void InputName()
+{
+	cout << "Please enter your first name: ";
+	cin >> fName;
+	cout << "\nPlease enter your last name: ";
+	cin >> lName;
+}
+
+void InputBirthDay()
+{
+	cout << "Please enter your birthday starting with month as a two digit number: ";
+	cin >> birthMonth;
+	cout << "\nPlease enter the day your were born as a two digit number: ";
+	cin >> birthDay;
+	cout << "\nPlease enter your birth year as a four digit number: ";
+	cin >> birthYear;
 }
